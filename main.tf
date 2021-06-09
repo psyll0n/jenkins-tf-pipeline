11dd324318ff716078397445e22348bee2091886
psyll0n <alex.jakimov@gmail.com>
2021-06-09 14:53:03 +0300

    Modified main.tf and provider.tf

--- main.tf
+++ main.tf
@@ -9,57 +9,6 @@ module "networking" {
 }
 
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "devops-tf-tfstate-backend"
-  # Enable versioning so we can see the full revision history of our
-  # state files
-  versioning {
-    enabled = true
-  }
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_policy" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Id": "MYBUCKETPOLICY",
-  "Statement": [
-    {
-      "Sid": "IPAllow",
-      "Effect": "Deny",
-      "Principal": "*",
-      "Action": "s3:*",
-      "Resource": "arn:aws:s3:::devops-tf-tfstate-backend/*",
-      "Condition": {
-         "IpAddress": {"aws:SourceIp": "0.0.0.0/0"}
-      }
-    }
-  ]
-}
-POLICY
-}
-
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "dynamodb-tf-tfstate-locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
 
 
 resource "aws_instance" "ubuntu" {
